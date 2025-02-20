23a925489abd0b0e1e9d64d64700b1dee64be755
Hieu Ngo <71876066+HieuNgoGH@users.noreply.github.com>
2025-02-19 17:36:36 -0800

    Add files via upload

---
+++ PowerSet.py
@@ -0,0 +1,22 @@
+import copy
+
+def powerset_helper(pointer, choices_made, input, result):
+    result1 = []
+    if (pointer < 0):
+        result1 = copy.copy(choices_made)
+        result.append(result1)
+        return result
+
+    choices_made.append(input[pointer])
+    powerset_helper(pointer-1, choices_made, input, result)
+    #backtracking
+    choices_made.pop()
+    powerset_helper(pointer-1, choices_made, input, result)
+
+def powerset(input):
+    result = []
+    powerset_helper(len(input)-1, [], input, result)
+    return result
+
+nums = [1,2,3]
+print(powerset(nums))
